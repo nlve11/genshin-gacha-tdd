32c5bcc891c479d8058ee292f09c90ee4b3f3b7e
刘沐阳 <3384068557@qq.com>
2026-05-03 23:20:47 +0800

    feat: pass all tests locally

--- business_logic.py
+++ business_logic.py
@@ -1,11 +1,22 @@
 def calculate_gacha_probability(pull_num: int) -> float:
     if pull_num <= 0:
-        raise ValueError
+        raise ValueError("Invalid pull number")
 
-    if pull_num >= 90:
-        return 1.0
-
-    if pull_num < 74:
+    # 超过90重置
+    if pull_num > 90:
         return 0.006
 
-    return 0.006 + (pull_num - 73) * 0.06
+    # 硬保底
+    if pull_num == 90:
+        return 1.0
+
+    # 基础概率
+    if pull_num <= 73:
+        return 0.006
+
+    # 软保底起点
+    if pull_num == 74:
+        return 0.066
+
+    # 软保底递增
+    return 0.066 + (pull_num - 74) * 0.06
